5b7ab39cb63246bfa860ebe7b969c46aa49e2be6
SrivatsanVasudevan <srivats1@uw.edu>
2020-09-09 15:02:56 -0700

    Completed the menu items in the homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component'
+
+
+const HomePage = () => {
+    return(
+        <div className = "homepage">
+            <DirectoryMenu />
+        </div>
+        )
+    }
+
+export default HomePage;
+
+
